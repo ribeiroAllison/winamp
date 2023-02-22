b44cb68ce535b5c5ce9f95ded7bf56dfc7675e64
ribeiroAllison <allisonribeiro@gmail.com>
2023-02-22 11:12:54 -0300

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+export default App;
